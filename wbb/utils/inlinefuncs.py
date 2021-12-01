0d1af946ec6da5c6d31f437fa2819488fdb6f64b
insidejob <84038103+insidescode@users.noreply.github.com>
2021-12-01 14:15:46 +0530

    Update inlinefuncs.py

--- wbb/utils/inlinefuncs.py
+++ wbb/utils/inlinefuncs.py
@@ -46,7 +46,7 @@ from wbb.core.keyboard import ikb
 from wbb.core.tasks import _get_tasks_text, all_tasks, rm_task
 from wbb.core.types import InlineQueryResultCachedDocument
 from wbb.modules.info import get_chat_info, get_user_info
-from wbb.modules.music import download_youtube_audio
+
 from wbb.utils.functions import test_speedtest
 from wbb.utils.pastebin import paste
 
